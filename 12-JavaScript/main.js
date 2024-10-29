8d82b8d7647fc6ec5615ccea78fde2ca50c88740
asgrlifarid <faridaskerli2@gmail.com>
2024-10-30 03:06:51 +0400

    not completed

---
+++ 12-JavaScript/main.js
@@ -0,0 +1,64 @@
+// let katet1=+prompt("1 ci kateti daxil edin");
+// let katet2=+prompt("2 ci kateti daxil edin");
+// let hipotenuz=+prompt("hipotenuzu daxil edin");
+
+
+// let sum=katet1*katet1+katet2*katet2
+// if(sum==hipotenuz*hipotenuz){
+//     console.log("duzbucaqli ucbucaqdir");
+// } else {
+//     console.log("duzbucaqli ucbucaq deyil");
+// }
+// let dayofMonth = +prompt("enter month!");
+
+
+// switch (dayofMonth) {
+//   case 1:
+//     console.log("January has 31!");
+//     break;
+//   case 2:
+//     console.log("February has 20!");
+//     break;
+//   case 3:
+//     console.log("Mart has 31!");
+//     break;
+//   case 4:
+//     console.log("April has 31!");
+//     break;
+//   case 5:
+//     console.log("May has 31!");
+//     break;
+//   case 6:
+//     console.log("June has 31!");
+//     break;
+//   case 7:
+//     console.log("July has 31!");
+//     break;
+//    case 8:
+//     console.log("August has 31!");
+//     break;
+//     case 9:
+//     console.log("September has 20!");
+//     break;
+//     case 10:
+//     console.log("October has 31!");
+//     break;
+//     case 11:
+//     console.log("is November has 31!");
+//     break;
+//     case 12:
+//     console.log("December has 31!");
+//     break;
+// }
+
+
+
+let number=+prompt("3 reqemli eded daxil edin")
+let first = (number);
+let second= (number % 100) / 10;  
+let third = number
+let reqem = `${first} ${second}`; 
+
+console.log("first: " + first);
+console.log("second: " + second);
+console.log("third: " + third);
