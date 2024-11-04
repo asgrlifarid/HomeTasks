6127ce3cc5510fc882c6939f1fac61ebfcc90ff9
AnarMuradov <mi82nr231@code.edu.az>
2024-11-04 19:22:38 +0400

    at class

---
+++ 15-JS-Task-4.11/main.js
@@ -0,0 +1,15 @@
+// function herflerBoyuyk(text) {
+//     return text.toUpperCase();
+// }
+
+// const result = herflerBoyuyk("Hello World");
+// console.log(result); 
+
+
+
+// function reverseTers(cumle) {
+//     return cumle.split(' ').reverse().join(' ');
+// }
+
+// const result = reverseTers("Hello World");
+// console.log(result); 
