cadcb45ee9033b01cfb01ae1e3e4c72a59b2e11e
asgrlifarid <faridaskerli2@gmail.com>
2024-11-16 13:17:38 +0530

    finitto

---
+++ 19-JS-Task-15.11/data.js
@@ -0,0 +1,30 @@
+let products = [
+  {
+    id: 1,
+    title: "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
+    price: 109.95,
+    description:
+      "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
+    image:
+      "https://hips.hearstapps.com/hmg-prod/images/2025-ford-mustang-60th-anniversary-exterior-66227932bb88e.jpg?crop=0.793xw:1.00xh;0.106xw,0&resize=1200:*",
+  },
+  {
+    id: 2,
+    title: "Mens Casual Premium Slim Fit T-Shirts ",
+    price: 22.3,
+    description:
+      "Slim-fitting style, contrast raglan long sleeve, three-button henley placket, light weight & soft fabric for breathable and comfortable wearing. And Solid stitched shirts with round neck made for durability and a great fit for casual fashion wear and diehard baseball fans. The Henley style round neckline includes a three-button placket.",
+    image: "https://i.ytimg.com/vi/ZNfEDcnuyUc/maxresdefault.jpg",
+  },
+  {
+    id: 3,
+    title: "Mens Cotton Jacket",
+    price: 55.99,
+    description:
+      "great outerwear jackets for Spring/Autumn/Winter, suitable for many occasions, such as working, hiking, camping, mountain/rock climbing, cycling, traveling or other outdoors. Good gift choice for you or your family member. A warm hearted love to Father, husband or son in this thanksgiving or Christmas Day.",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTL7pm2LMbFW3why8rCBOXspp3FhZuMK_XUV5l8MJ3ZuwL2UULEO7wQOuqXyoqB6xepJOA&usqp=CAU",
+  },
+];
+
+export {products};
--- 19-JS-Task-15.11/form.js
+++ 19-JS-Task-15.11/form.js
@@ -1,3 +1,5 @@
+import { products } from "./data.js";
+
 const submitButton = document.getElementById("submitBtn");
 const closeErrorButton = document.getElementById("closeErrorModal");
 const closeUgurluButton = document.getElementById("closeUgurluModal");
@@ -8,7 +10,6 @@ submitButton.addEventListener("click", function () {
   const description = document.getElementById("description").value;
   const image = document.getElementById("image").value;
 
-  
   if (title === "" || price === "" || description === "" || image === "") {
     document.getElementById("error").style.display = "block";
   } else {
@@ -23,3 +24,117 @@ closeErrorButton.addEventListener("click", function () {
 closeUgurluButton.addEventListener("click", function () {
   document.getElementById("ugurlu").style.display = "none";
 });
+
+const tBody = document.querySelector("tbody");
+const searchInput = document.querySelector("#search");
+function drawTable(array) {
+  tBody.innerHTML = "";
+  array.forEach((product) => {
+    const trElem = document.createElement("tr");
+    trElem.innerHTML = `
+      
+            <td>${product.id}</td>
+            <td>${product.title}</td>
+            <td>${product.price}</td>
+            <td>${product.description}</td>
+               <td><img src="${product.image}" width="100"/></td>
+            <td><i class="fa-solid fa-trash-can text-danger delete-btn" data-id=${product.id}></i></td>
+        `;
+
+    tBody.appendChild(trElem);
+  });
+}
+searchInput.addEventListener("input", function (event) {
+  console.log(event.target.value);
+
+  const filteredProducts = products.filter((product) =>
+    product.title.toLowerCase().includes(event.target.value.trim())
+  );
+
+  drawTable(filteredProducts);
+});
+drawTable(products);
+
+const allDeleteBtns = document.querySelectorAll(".delete-btn");
+allDeleteBtns.forEach((btn) => {
+  btn.addEventListener("click", function () {
+    const productId = this.getAttribute("data-id");
+    const idx = products.findIndex((item) => item.id == productId);
+
+    Swal.fire({
+      title: "Are you sure to delete product!?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        deleteProduct(idx, this);
+        Swal.fire({
+          title: "Deleted!",
+          text: "Your file has been deleted.",
+          icon: "success",
+        });
+      }
+    });
+  });
+});
+
+drawTable(products);
+
+function deleteProduct(index, btn) {
+  products.splice(index, 1);
+  console.log(products);
+  btn.closest("tr").remove();
+  drawTable(products);
+}
+
+const ascBtn = document.querySelector(".asc");
+const descBtn = document.querySelector(".desc");
+const defaultBtn = document.querySelector(".default");
+const sortBtn = document.querySelector(".sort");
+
+const productForm = document.querySelector(".product-form");
+
+
+
+// searchInput.addEventListener("input", function (event) {
+//   const filteredProducts = products.filter((product) =>
+//     product.title.toLowerCase().includes(event.target.value.trim())
+//   );
+
+//   drawTable(filteredProducts);
+// });
+
+// ascBtn.addEventListener("click", function () {
+//   const sortedProducts = products.toSorted((a, b) => a.price - b.price);
+//   drawTable(sortedProducts);
+// });
+// descBtn.addEventListener("click", function () {
+//   const sortedProducts = products.toSorted((a, b) => b.price - a.price);
+//   drawTable(sortedProducts);
+// });
+// defaultBtn.addEventListener("click", function () {
+//   drawTable(products);
+// });
+
+// sortBtn.addEventListener("click", function () {
+//   let sorted;
+//   if (this.textContent === "ASC by Price") {
+//     this.textContent = "DESC by Price";
+//     sorted = products.toSorted((a, b) => a.price - b.price);
+//   } else if (this.textContent === "DESC by Price") {
+//     this.textContent = "Default";
+//     sorted = products.toSorted((a, b) => b.price - a.price);
+//   } else {
+//     this.textContent = "ASC by Price";
+//     sorted = structuredClone(products);
+//   }
+//   drawTable(sorted);
+// });
+
+// productForm.addEventListener("submit", function (e) {
+//   e.preventDefault();
+// });
