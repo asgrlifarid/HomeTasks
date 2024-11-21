5cef31f18540d226c2c0afce27e97857b7bc060b
asgrlifarid <faridaskerli2@gmail.com>
2024-11-21 18:35:34 +0400

    class 50% finish

--- 21-JS-Task-20.11/helpers.js
+++
@@ -1,6 +0,0 @@
-export function getDataFromLocalStorage(key) {
-    return JSON.parse(localStorage.getItem(key));
-  }
-  export function setDataToLocalStorage(key, data) {
-    localStorage.setItem(key, JSON.stringify(data));
-  }
--- 21-JS-Task-21.11/js/script.js
+++ 21-JS-Task-21.11/js/script.js
@@ -1,1 +1,22 @@
-import { carsData } from "./data.js";
+import { carsData } from './data.js'; 
+
+const carList = document.getElementById('car-list'); 
+
+carsData.forEach(car => {
+  const carItem = document.createElement('div');
+  carItem.classList.add('car-item', 'mb-3');
+  carItem.innerHTML = `
+    <div class="card   ">
+      <img src="${car.img}" class="card-img-top " alt="${car.name} image ">  <!-- Add the image here -->
+      <div class="card-body">
+        <h5 class="card-title">${car.name} - ${car.type}</h5>
+        <p class="card-text">Yanacaq: ${car.fuel}</p>
+        <p class="card-text">Transport: ${car.transmission}</p>
+        <p class="card-text">Yolçular: ${car.passengers}</p>    
+        <p class="card-text">Qiymət: $${car.price}</p>
+        ${car.discount ? `<p class="card-text">Endirim: ${car.discount}</p>` : ''}
+      </div>
+    </div>
+  `;
+  carList.appendChild(carItem); 
+});
